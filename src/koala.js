61090a7385bdd4fffb4b20ea9f081b6d511e2315
10012 <nas7062@naver.com>
2025-04-19 22:12:41 +0900

    앨범 만들기

---
+++ src/koala.js
@@ -0,0 +1,19 @@
+const API_URL = "https://animal-api-two.vercel.app/";
+const $content = document.querySelector(".content");
+let template = [];
+const getData = async (name) => {
+  let res = await fetch(`${API_URL}${name}`);
+  try {
+    if (res) {
+      let data = await res.json();
+      data.photos.forEach((photo) => {
+        template += `<img src=${photo.url} />`;
+      });
+      $content.innerHTML = template;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData(`koala`);
