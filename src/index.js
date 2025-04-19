3968c4d78c10ef1f9013ea5fd78c800d7bf2ad4c
10012 <nas7062@naver.com>
2025-04-19 21:43:45 +0900

    동물 앨범 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
+const API_URL = "https://animal-api-two.vercel.app/";
+const $content = document.querySelector(".content");
+let template = [];
+const getData = async () => {
+  let res = await fetch(API_URL);
+  try {
+    if (res) {
+      let data = await res.json();
+      data.photos.forEach((photo) => {
+        template += `<img src=${photo.url} />`;
+      });
+      $content.innerHTML = template;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const $color = document.getElementById('color');
-const $animalInfo = document.querySelector('.animal-info');
-console.log($color);
+getData();
