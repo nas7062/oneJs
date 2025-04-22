2d6a0a735bb4c995b4f0bb7c2ef95f9f75b7aa55
10012 <nas7062@naver.com>
2025-04-22 21:55:09 +0900

    2025 04 22

--- src/App.js
+++ src/App.js
@@ -1,48 +1,48 @@
-import TabBar from './components/TabBar.js';
-import Content from './components/Contents.js';
+import TabBar from "./components/TabBar.js";
+import Content from "./components/Contents.js";
 
-import { request } from './components/api.js';
+import { request } from "./components/api.js";
 
 export default function App($app) {
-    this.state = {
-        currentTab: 'all',
-        photos: [],
-    };
+  this.state = {
+    currentTab: window.location.pathname.replace("/", "") || "all",
+    photos: [],
+  };
 
-    //tab
-    const tab = new TabBar({
-        $app,
-        initialState: '',
-        onClick: async (name) => {
-            this.setState({
-                ...this.state,
-                currentTab: name,
-                photos: await request(name === 'all' ? '' : name),
-            });
-        },
+  //tab
+  const tab = new TabBar({
+    $app,
+    initialState: "",
+    onClick: async (name) => {
+      history.pushState(null, `${name}사진`, name);
+      this.setState({
+        ...this.state,
+        currentTab: name,
+        photos: await request(name === "all" ? "" : name),
+      });
+    },
+  });
+
+  //content
+  const content = new Content({ $app, initialState: [] });
+
+  //state
+  this.setState = (newState) => {
+    this.state = newState;
+    tab.setState(this.state.currentTab);
+    content.setState(this.state.photos);
+  };
+
+  window.addEventListener("popstate", async () => {
+    const tabName = window.location.pathname.replace("/", "") || "all";
+    const photos = await request(tabName === "all" ? " " : tabName);
+    this.setState({
+      ...this.state,
+      currentTab: tabName,
+      photos: photos,
     });
+  });
+  const init = async () => {};
 
-    //content
-    const content = new Content({ $app, initialState: [] });
-
-    //state
-    this.setState = (newState) => {
-        this.state = newState;
-        tab.setState(this.state.currentTab);
-        content.setState(this.state.photos);
-    };
-
-    const init = async () => {
-        try {
-            const initialPhotos = await request();
-            this.setState({
-                ...this.state,
-                photos: initialPhotos,
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    init();
+  init();
 }
